30b15c1ef07110bf2a0d9e3171d963094fb650a2
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-12-13 00:58:10 -0800

    make poly load background and blocks :O

--- poly.lua
+++ poly.lua
@@ -1,8 +1,31 @@
+local common = require 'common'
+
+
+--- VARIABLES
+
+-- Physics
 local world
 
-local floors
+-- Level
+local level
+
+-- View
+local viewX, viewY
+
+-- Background
+local bg
+
+-- Solids
+local solids
+
+-- Player
 local player
+
+-- Ball
 local ball
+
+
+--- LOAD
 
 function love.load()
     do -- Physics
@@ -10,41 +33,45 @@ function love.load()
         world = love.physics.newWorld(0, 32 * 64, true)
     end
 
-    do -- Floors
-        floors = {}
+    do -- Level
+        level = 1
+    end
 
-        do
-            floors[1] = {}
-            floors[1].body = love.physics.newBody(world, 20, 20)
-            floors[1].shape = love.physics.newChainShape(false, 0, 200, 200, 200, 400, 200)
-            floors[1].fixture = love.physics.newFixture(floors[1].body, floors[1].shape)
-        end
+    do -- View
+        viewX, viewY = 0, 0
+    end
 
-        do
-            floors[2] = {}
-            floors[2].body = love.physics.newBody(world, 400, 300, 'kinematic')
-            floors[2].shape = love.physics.newChainShape(false, 0, 200, 200, 200, 400, 120)
-            floors[2].fixture = love.physics.newFixture(floors[2].body, floors[2].shape)
-        end
+    do -- Background
+        bg = common.loadBg(level)
+    end
 
-        do
-            floors[3] = {}
-            floors[3].body = love.physics.newBody(world, 200, 500, 'kinematic')
-            floors[3].shape = love.physics.newChainShape(false, 0, 200, 200, 200, 400, 180)
-            floors[3].fixture = love.physics.newFixture(floors[3].body, floors[3].shape)
-        end
+    local blocks
+    do -- Blocks
+        blocks = common.loadBlocks(level)
+    end
 
-        do
-            floors[4] = {}
-            floors[4].body = love.physics.newBody(world, -100, 450, 'kinematic')
-            floors[4].shape = love.physics.newChainShape(false, 0, 200, 200, 200, 400, 180)
-            floors[4].fixture = love.physics.newFixture(floors[4].body, floors[4].shape)
+    do -- Solids
+        solids = {}
+        for _, b in pairs(blocks) do
+            if b.type == 'solid' then
+                local solid = {}
+                solid.body = love.physics.newBody(world, b.x, b.y)
+                solid.shape = love.physics.newChainShape(false, unpack(b.points))
+                solid.fixture = love.physics.newFixture(solid.body, solid.shape)
+                table.insert(solids, solid)
+            end
         end
     end
 
     do -- Player
         player = {}
-        player.body = love.physics.newBody(world, 80, 0, 'dynamic')
+        local x, y = 0, 0
+        for _, b in pairs(blocks) do
+            if b.type == 'spawn' then
+                x, y = b.x, b.y
+            end
+        end
+        player.body = love.physics.newBody(world, x, y, 'dynamic')
         player.shape = love.physics.newRectangleShape(32, 90)
         player.fixture = love.physics.newFixture(player.body, player.shape, 0)
         player.fixture:setFriction(0.4)
@@ -56,66 +83,102 @@ function love.load()
 
     do -- Ball
         ball = {}
-        ball.body = love.physics.newBody(world, 400, 0, 'dynamic')
+        local px, py = player.body:getPosition()
+        ball.body = love.physics.newBody(world, px + 40, py, 'dynamic')
         ball.shape = love.physics.newCircleShape(14)
         ball.fixture = love.physics.newFixture(ball.body, ball.shape, 0)
         ball.fixture:setRestitution(0.3)
     end
 end
 
-local characterImg
-local characterQuad
-characterImg = love.graphics.newImage('assets/character-1.png')
-characterQuad = love.graphics.newQuad(0, 0, 72, 126, characterImg:getDimensions())
-local characterFlip = 1
 
-function love.draw()
-    do -- World
-        do -- Player
---            love.graphics.polygon('line', player.body:getWorldPoints(player.shape:getPoints()))
-            local x, y = player.body:getPosition()
-            local vx, vy = player.body:getLinearVelocity()
-            local left, right = love.keyboard.isDown('left'), love.keyboard.isDown('right')
-            local walking = (left or right) and math.abs(vx) >= 0.01
-            if walking then
-                if left then
-                    characterFlip = -1
-                elseif right then
-                    characterFlip = 1
+--- DRAW
+
+do
+    local characterImg = love.graphics.newImage('assets/character-1.png')
+    local characterQuad = love.graphics.newQuad(0, 0, 72, 126, characterImg:getDimensions())
+    local characterFlip = 1
+
+    function love.draw()
+        do -- World
+            love.graphics.stacked('all', function()
+                do -- View
+                    local x, y = player.body:getPosition()
+                    local ww, wh = love.graphics.getDimensions()
+                    if x - 240 < viewX then
+                        viewX = x - 240
+                    end
+                    if x + 240 > viewX + ww then
+                        viewX = x + 240 - ww
+                    end
+                    if y - 240 < viewY then
+                        viewY = y - 240
+                    end
+                    if y + 240 > viewY + wh then
+                        viewY = y + 240 - wh
+                    end
+                    love.graphics.translate(-viewX, -viewY)
                 end
-                characterQuad:setViewport(72 * (math.floor(10 * love.timer.getTime()) % 6), 126, 72, 126)
-            else
-                local sq = 1 - math.abs(math.sin(0.7 * love.timer.getTime()))
-                sq = sq * sq
-                characterQuad:setViewport(72 * (5 - math.floor(4 * sq * sq * sq * sq)), 0, 72, 126)
+
+                do -- Background
+                    love.graphics.draw(bg, 0, 0)
+                end
+
+                do -- Player
+--                    love.graphics.polygon('line', player.body:getWorldPoints(player.shape:getPoints()))
+                    local x, y = player.body:getPosition()
+                    local vx, vy = player.body:getLinearVelocity()
+                    local left, right = love.keyboard.isDown('left'), love.keyboard.isDown('right')
+                    local walking = (left or right) and math.abs(vx) >= 0.01
+                    if walking then
+                        if left then
+                            characterFlip = -1
+                        elseif right then
+                            characterFlip = 1
+                        end
+                        local frame = math.floor(10 * love.timer.getTime()) % 6
+                        characterQuad:setViewport(72 * frame, 126, 72, 126)
+                    else
+                        local sq = 1 - math.abs(math.sin(0.7 * love.timer.getTime()))
+                        sq = sq * sq
+                        local frame = 5 - math.floor(4 * sq * sq * sq * sq)
+                        characterQuad:setViewport(72 * frame, 0, 72, 126)
+                    end
+                    love.graphics.draw(characterImg, characterQuad,
+                        x + 72 * (0.5 * -characterFlip + 0.5) - 36, y - 32 - 45,
+                        0, characterFlip, 1)
+                end
+
+                do -- Ball
+                    love.graphics.circle('fill', ball.body:getX(), ball.body:getY(), ball.shape:getRadius())
+                end
+
+                do -- Solids
+--                    love.graphics.stacked('all', function()
+--                        love.graphics.setLineWidth(3)
+--                        for _, solid in pairs(solids) do
+--                            love.graphics.line(solid.body:getWorldPoints(solid.shape:getPoints()))
+--                        end
+--                    end)
+                end
+            end)
+        end
+
+        do -- Stats
+            love.graphics.print('fps: ' .. love.timer.getFPS(), 20, 20)
+
+            if love.keyboard.isDown('left') then
+                love.graphics.print('\n\nLEFT pressed', 20, 20)
             end
-            love.graphics.draw(characterImg, characterQuad,
-                x + 72 * (0.5 * -characterFlip + 0.5) - 36, y - 32 - 45,
-                0, characterFlip, 1)
-        end
-
-        do -- Ball
-            love.graphics.circle('fill', ball.body:getX(), ball.body:getY(), ball.shape:getRadius())
-        end
-
-        do -- Floors
-            for _, floor in pairs(floors) do
-                love.graphics.line(floor.body:getWorldPoints(floor.shape:getPoints()))
+            if love.keyboard.isDown('right') then
+                love.graphics.print('\n\n\nRIGHT pressed', 20, 20)
             end
-        end
-    end
-
-    do -- Stats
-        love.graphics.print('fps: ' .. love.timer.getFPS(), 20, 20)
-
-        if love.keyboard.isDown('left') then
-            love.graphics.print('\n\nLEFT pressed', 20, 20)
-        end
-        if love.keyboard.isDown('right') then
-            love.graphics.print('\n\n\nRIGHT pressed', 20, 20)
         end
     end
 end
+
+
+--- UPDATE
 
 function love.update(dt)
     do -- Player
@@ -172,16 +235,19 @@ function love.update(dt)
         end
     end
 
-    do -- Moving floors
-        floors[2].body:setLinearVelocity(40 * math.sin(love.timer.getTime()), 0)
-        floors[3].body:setLinearVelocity(0, 40 * math.sin(love.timer.getTime()))
-        floors[4].body:setLinearVelocity(40 * math.sin(love.timer.getTime()), 0)
-    end
+--    do -- Moving solids
+--        solids[2].body:setLinearVelocity(40 * math.sin(love.timer.getTime()), 0)
+--        solids[3].body:setLinearVelocity(0, 40 * math.sin(love.timer.getTime()))
+--        solids[4].body:setLinearVelocity(40 * math.sin(love.timer.getTime()), 0)
+--    end
 
     do -- Physics
         world:update(dt)
     end
 end
+
+
+--- KEYBOARD
 
 function love.keypressed(key)
     do -- Player
